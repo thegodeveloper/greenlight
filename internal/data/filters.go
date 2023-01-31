739ed49f3de9dbbe6ec018c7c371e81bcea9beb4
William Munoz <william.munozr@gmail.com>
2023-01-31 11:18:27 -0500

    add paginating lists

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -38,3 +38,12 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
