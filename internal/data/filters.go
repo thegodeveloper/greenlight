b9961f2a50c39eff96b1d94b1081f2d4fe8d6f00
William Munoz <william.munozr@gmail.com>
2023-01-31 12:18:35 -0500

    add returning pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,10 +1,19 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"github.com/williammunozr/greenlight/internal/validator"
 )
+
+type Metadata struct {
+	CurrentPage int `json:"current_page,omitempty"`
+	PageSize int `json:"page_size,omitempty"`
+	FirstPage int `json:"first_page,omitempty"`
+	LastPage int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
 
 type Filters struct {
 	Page         int
@@ -45,5 +54,19 @@ func (f Filters) limit() int {
 
 func (f Filters) offset() int {
 	return (f.Page - 1) * f.PageSize
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage: page,
+		PageSize: pageSize,
+		FirstPage: 1,
+		LastPage: int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
 
