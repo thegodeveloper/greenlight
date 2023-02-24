9bd0d3182ac827113db19e85f9557cf77e2c79b0
William Munoz <william.munozr@gmail.com>
2023-02-24 18:22:32 -0500

    adding a custom logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,14 +5,14 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/williammunozr/greenlight/internal/jsonlog"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/williammunozr/greenlight/internal/data"
-
 	_ "github.com/lib/pq"
+	"github.com/williammunozr/greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -31,7 +31,7 @@ type config struct {
 // Add a models field to hold our new Models struct.
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -47,15 +47,15 @@ func main() {
 
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	// Use the data.NewModels() function to initialize a Models struct, passing in the
 	// connection pool as a parameter.
@@ -68,14 +68,18 @@ func main() {
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		ErrorLog:     log.New(logger, "", 0),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
