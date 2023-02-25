a4e42b6542e15c144e1bb00487e110dea17ae13d
William Munoz <william.munozr@gmail.com>
2023-02-25 08:21:04 -0500

    executing the shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"github.com/williammunozr/greenlight/internal/jsonlog"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -61,8 +58,8 @@ func main() {
 	if err != nil {
 		logger.PrintFatal(err, nil)
 	}
-
 	defer db.Close()
+
 	logger.PrintInfo("database connection pool established", nil)
 
 	// Use the data.NewModels() function to initialize a Models struct, passing in the
@@ -73,21 +70,10 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
