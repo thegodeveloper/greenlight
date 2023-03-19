759d5ac5cc808e129da953e5b34e16ef54dc2b72
william <william.munozr@gmail.com>
2023-03-19 18:11:05 -0500

    add simple cors requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/williammunozr/greenlight/internal/jsonlog"
 	"github.com/williammunozr/greenlight/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 // Add a models field to hold our new Models struct.
@@ -65,6 +69,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "5d62698c7c1943", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "8ab44301bfc525", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@godeveloper.io>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
